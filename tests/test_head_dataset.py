9521881c9d21a1c616d1098ceee9ac101a704610
alwaysT <alwaysT2018@foxmail.com>
2019-03-26 20:34:56 +0800

    Create test_head_dataset.py

---
+++ tests/test_head_dataset.py
@@ -0,0 +1,13 @@
+from maskrcnn_benchmark.data.datasets import HeadDataset
+
+
+def main():
+    data_dir = "/workspace/csf/data/head_detect/train/yuncong_data/"
+    gt_file = 'UCSD_train.txt'
+    head = HeadDataset(data_dir, gt_file)
+    from ipdb import set_trace
+    set_trace()
+    print('test')
+
+if __name__ == '__main__':
+    main()
